f6721e829a5a3b113f91f766991695adcec1fd71
kampkelly <kampkellykeys@gmail.com>
2017-07-29 23:06:56 +0100

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -247,7 +247,7 @@
 	</div>
 
 </section>
-<div style="height: 1em;"></div>
+<div style="height: 2em;"></div>
 @endsection
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
