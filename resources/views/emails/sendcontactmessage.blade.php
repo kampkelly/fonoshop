ccb66319422620ec448b3429286b9882f6952510
kampkelly <kampkellykeys@gmail.com>
2017-08-19 13:12:06 +0100

    fixes

--- resources/views/emails/sendcontactmessage.blade.php
+++ resources/views/emails/sendcontactmessage.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<h3 class="text-center">New Message From </h3>
-<p class="text-center text-justify">Message</p>
+<h3 class="text-center">New Message From {{$email_data->contact_name}}</h3>
+<p class="text-center text-justify">{{$email_data->contact_message}}</p>
 <a href="fonoshop.dev" class="btn btn-primary">Home Page</a>
 
 <div>
