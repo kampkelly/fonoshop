c20fdb0253145f33d6a08f98ab71d2c93e6ee4a9
kampkelly <kampkellykeys@gmail.com>
2017-08-29 15:27:43 +0100

    fixed mobile links

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -32,9 +32,10 @@
 			</section>
 		</div>
 		<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
-		<h6 style="cursor: pointer;" class="btn btn-sm btn-info show-contact">Show Seller's Contact</h6>
+	<!--	<h6 style="cursor: pointer;" class="btn btn-sm btn-info show-contact">Show Seller's Contact</h6> -->
+		<h6 style="cursor: pointer;" class="btn btn-sm btn-info show-contact">Seller's Contact</h6>
 		<h6 style="cursor: pointer; display: none;" class="btn btn-sm btn-warning hide-contact">Hide Seller Contact</h6>
-		<p class="small contact" style="display: none;">Phone: {{$product->phone}}<br> Address: {{$product->state}}, {{$product->city}}</p>
+		<p class="small contact" style="dislay: none;">Phone: {{$product->phone}}<br> Address: {{$product->state}}, {{$product->city}}</p>
 		</div>
 	</div>
 	</div>
